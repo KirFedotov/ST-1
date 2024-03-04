ca34363f0a4e4669bb50fa602e16797b7c63f4f3
KirFedotov <k.fedotov.guit29@mail.ru>
2024-03-04 20:43:45 +0300

    test3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@
 
 
 bool checkPrime(uint64_t value) {
- if (value <= 1)
-    return false;
-  for (uint64_t i = 2; i*i <= value; i++) {
-    if (value % i == 0)
-      return false;
-  }
-  return true;
+    if (value <= 1)
+        return false;
+    for (uint64_t i = 2; i*i <= value; i++) {
+        if (value % i == 0)
+        return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -36,11 +36,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
- uint64_t sum = 0;
-  for (int i = 0; i < hbound; i++) {
-    if (checkPrime(i)) {
-      sum += i;
+    uint64_t sum = 0;
+    for (int i = 0; i < hbound; i++) {
+        if (checkPrime(i)) {
+        sum += i;
+        }
     }
-  }
-  return sum;
+    return sum;
 }
