ffb2a572c0349a4ac524af04f6dfea76acc4d77b
KirFedotov <k.fedotov.guit29@mail.ru>
2024-03-04 20:49:14 +0300

    test5

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,52 +3,52 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(tests, checkPrime_val) { 
+TEST(tests, checkPrime_val) {
   EXPECT_TRUE(checkPrime(11));
 }
 
-TEST(tests, checkPrime_inval_1) { 
+TEST(tests, checkPrime_inval_1) {
   EXPECT_FALSE(checkPrime(10));
 }
 
-TEST(tests, checkPrime_inval_2) { 
+TEST(tests, checkPrime_inval_2) {
   EXPECT_FALSE(checkPrime(0));
 }
 
-TEST(tests, checkPrime_inval_3) { 
+TEST(tests, checkPrime_inval_3) {
   EXPECT_FALSE(checkPrime(1));
 }
 
-TEST(tests, nPrime_val) { 
+TEST(tests, nPrime_val) {
   EXPECT_EQ(nPrime(5), 3);
 }
 
-TEST(tests, nPrime_inval) {  
+TEST(tests, nPrime_inval) {
   EXPECT_NE(nPrime(2), 2);
 }
 
-TEST(tests, nextPrime_val) {  
+TEST(tests, nextPrime_val) {
   EXPECT_EQ(nPrime(11), 13);
 }
 
-TEST(tests, nextPrime_inval) {  
+TEST(tests, nextPrime_inval) {
   EXPECT_NE(nPrime(11), 12);
 }
 
-TEST(tests, sumPrime_val) {  
+TEST(tests, sumPrime_val) {
   EXPECT_EQ(sumPrime(17), 41);
 }
 
 
-TEST(tests, sumPrime_inval) { 
+TEST(tests, sumPrime_inval) {
   EXPECT_NE(sumPrime(17), 58);
 }
 
-TEST(tests, sumPrime_inval) { 
+TEST(tests, sumPrime_inval) {
   EXPECT_NE(sumPrime(17), 68);
 }
 
-TEST(tests, task_2) {  
+TEST(tests, task_2) {
   EXPECT_NE(sumPrime(2000000), 142913828922);
 }
 
